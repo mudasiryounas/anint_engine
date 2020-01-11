3cbf6bc6535d58f865fa9b007284f3c113f1b637
mudasiryounas <mudasir@sarp.io>
2020-01-11 17:50:59 +0300

    checkpoint

---
+++ core/daemons/job_scheduler_daemon.py
@@ -0,0 +1,34 @@
+# Copyright (C) 2020 Android Intelligence - All Rights Reserved
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential
+import os
+import time
+
+from core import db_session
+from core.db_models import AppsToFollow
+from core.jobs.check_for_app_updates import check_for_app_updates
+
+
+def job_scheduler_daemon():
+    print(f"Job scheduler daemon started for first time")
+    while True:
+        try:
+            sleep_seconds = 10
+            apps_to_follow = db_session.query(AppsToFollow).all()
+            apps_to_follow_packages = [item.package for item in apps_to_follow]
+            print(f"Adding '{len(apps_to_follow_packages)}' apps to queue for checking updates")
+            for package in apps_to_follow_packages:
+                # todo make it queue
+                check_for_app_updates(package)
+            print(f"Sleeping {sleep_seconds} seconds before triggering a new jobs")
+            time.sleep(sleep_seconds)
+        except KeyboardInterrupt as e:
+            print("Keyboard interrupt is taken, stopping the job scheduler daemon.")
+            break
+        except Exception as e:
+            print(f"Exception at job scheduler daemon, Exception: '{str(e)}'")
+            os._exit(-1)
+
+
+if __name__ == '__main__':
+    job_scheduler_daemon()
