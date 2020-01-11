c1086727cdcaa2e44ad77818fa79257f1356caea
mudasiryounas <mudasir@sarp.io>
2020-01-12 00:54:34 +0300

    check_for_new_released_apps job

--- core/jobs/check_for_app_updates.py
+++ core/jobs/check_for_app_updates.py
@@ -5,26 +5,30 @@ from core.utils.app_utils import AppUtils
 from core.utils.playstore_utils import PlaystoreUtils
 
 
+def download_latest_apk(package, version):
+    pass
+
+
 def check_for_app_updates(package):
     current_app_on_db = AppUtils.get_app(package)
-    continue_job = False
+    new_version = None
     if current_app_on_db is None:
         print(f"Job is running first time for app: '{package}'")
-        AppUtils.add_new_app(package)
-        continue_job = True
+        added_app = AppUtils.add_new_app(package)
+        new_version = added_app.current_version
     else:
         # check if application version is updated from playstore
         latest_playstore_app = PlaystoreUtils.get_app_latest_info(package)
         if current_app_on_db.current_version != latest_playstore_app['current_version']:
             # if application version is updated, process latest apk
-            print(f"New version is detected for app: '{package}'")
-            continue_job = True
+            print(f"New version is detected for app: '{package}', new version: '{latest_playstore_app['current_version']}'")
+            new_version = latest_playstore_app['current_version']
         # update application details on db, even if version is not updated, in order to keep application details history
         AppUtils.update_app(package, latest_playstore_app)
 
-    if continue_job:
-        print(f"Following app will be downloaded, decompiled and processed, app: '{package}'")
-        # download latest apk
+    if new_version:
+        print(f"Following app will be downloaded, decompiled and processed, app: '{package}', version: '{new_version}'")
+        download_latest_apk(package, new_version)
         # decompile latest apk
         # process latest decompiled files
     else:
